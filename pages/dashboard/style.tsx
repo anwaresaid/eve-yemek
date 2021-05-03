109a32c94d1462581f3dba6588b31a6fbce59d43
abdulazizcode <tajikcoder@gmail.com>
2021-05-03 21:42:00 +0800

    feature/SB-130 fixed responsive

--- pages/dashboard/style.tsx
+++ pages/dashboard/style.tsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 export const DashboardWrapper = styled.div`
   .box {
-    text-align: center;
     border-radius: 4px;
     box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2),
       0 1px 1px 0 rgba(0, 0, 0, 0.14), 0 1px 3px 0 rgba(0, 0, 0, 0.12);
@@ -13,6 +12,8 @@ export const DashboardWrapper = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    height:120px;
+    position:relative;
   }
   .box__info {
     display: flex;
@@ -21,14 +22,14 @@ export const DashboardWrapper = styled.div`
     color: #fff;
   }
   .box__info p {
-    font-size: 1rem;
-    line-height: 1.5;
+    font-size: 18px;
+    line-height: 1;
   }
   .box__info span {
     font-size: 30px;
     font-weight: bolder;
     margin-bottom: 10px;
-    margin-left: -50px;
+   
   }
   .box__icons {
     margin-right: 20px;
@@ -36,6 +37,10 @@ export const DashboardWrapper = styled.div`
   .box__icons i {
     opacity: 0.2;
     color: #495057;
-    font-size: 4em;
+    font-size: 2em;
+    position:absolute;
+    top:20px;
+    right:20px;
+    font-size:80px;
   }
 `;
