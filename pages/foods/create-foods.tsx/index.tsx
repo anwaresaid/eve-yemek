347f444163bbf1b9ebca3b6119ccf940dbd90073
anwaresaid <51533569+anwaresaid@users.noreply.github.com>
2021-05-07 11:08:57 +0300

    completed the design create page

--- pages/foods/create-foods.tsx/index.tsx
+++ pages/foods/create-foods.tsx/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Toast } from 'primereact/toast';
 import { FileUpload } from 'primereact/fileupload';
 import { ProgressBar } from 'primereact/progressbar';
@@ -6,16 +6,34 @@ import { Button } from 'primereact/button';
 import {InputText} from 'primereact/inputtext';
 import { Tag } from 'primereact/tag';
 import * as S from './style'
-import { DataTable } from 'primereact/datatable';
+import { Dropdown } from 'primereact/dropdown';
+import { InputNumber } from 'primereact/inputnumber';
+import { InputSwitch } from 'primereact/inputswitch';
 
 export const CreatFoods = () => {
     const [totalSize, setTotalSize] = useState(0);
+    const [files, setFile] = useState([]);
     const toast = useRef(null);
     const fileUploadRef = useRef(null);
+    const [value17, setValue17] = useState(20);
+    const [vegi, setVegi] = useState(false);
+    const [featured, setFeatured] = useState(false);
+    const [active, setActive] = useState(false);
 
+
+    const [selectedCity1, setSelectedCity1] = useState(null);
+    const cities = [
+        { name: 'New York', code: 'NY' },
+        { name: 'Rome', code: 'RM' },
+        { name: 'London', code: 'LDN' },
+        { name: 'Istanbul', code: 'IST' },
+        { name: 'Paris', code: 'PRS' }
+    ];
+    const onCityChange = (e) => {
+        setSelectedCity1(e.value);
+    }
 
     const onTemplateSelect = (e) => {
-        console.log(e.files);
         let _totalSize = totalSize;
         // e.files.map(file => {
         //     _totalSize += file.size;
@@ -24,11 +42,13 @@ export const CreatFoods = () => {
             _totalSize += e.files[i].size;
         }
         setTotalSize(_totalSize);
+
     }
+
 
     const onTemplateUpload = (e) => {
         let _totalSize = 0;
-        console.log(e.files);
+        setFile(e.files);
         e.files.forEach(file => {
             _totalSize += (file.size || 0);
         });
@@ -45,9 +65,10 @@ export const CreatFoods = () => {
     const onTemplateClear = () => {
         setTotalSize(0);
     }
+
     const headerTemplate = (options) => {
         const { className, chooseButton, uploadButton, cancelButton } = options;
-        const value = totalSize/10000;
+        const value = totalSize/20000;
         const formatedValue = fileUploadRef && fileUploadRef.current ? fileUploadRef.current.formatSize(totalSize) : '0 B';
 
         return (
@@ -55,7 +76,7 @@ export const CreatFoods = () => {
                 {chooseButton}
                 {uploadButton}
                 {cancelButton}
-                <ProgressBar value={value} displayValueTemplate={() => `${formatedValue} / 1 MB`} style={{width: '300px', height: '20px', marginLeft: 'auto'}}></ProgressBar>
+                <ProgressBar value={value} displayValueTemplate={() => `${formatedValue} / 2 MB`} style={{width: '300px', height: '20px', marginLeft: 'auto'}}></ProgressBar>
             </div>
         );
     }
@@ -79,7 +100,7 @@ export const CreatFoods = () => {
     const emptyTemplate = () => {
         return (
             <div className="p-d-flex p-ai-center p-dir-col">
-                <i className="pi pi-image p-mt-3 p-p-5" style={{'fontSize': '5em', borderRadius: '50%', backgroundColor: 'var(--surface-b)', color: 'var(--surface-d)'}}></i>
+                 <i className="pi pi-image p-mt-3 p-p-5" style={{'fontSize': '5em', borderRadius: '50%', backgroundColor: 'var(--surface-b)', color: 'var(--surface-d)'}}></i>
                 <span style={{'fontSize': '1.2em', color: 'var(--text-color-secondary)'}} className="p-my-5">Drag and Drop Image Here</span>
             </div>
         )
@@ -88,37 +109,66 @@ export const CreatFoods = () => {
     const chooseOptions = {icon: 'pi pi-fw pi-images', iconOnly: true, className: 'custom-choose-btn p-button-rounded p-button-outlined'};
     const uploadOptions = {icon: 'pi pi-fw pi-cloud-upload', iconOnly: true, className: 'custom-upload-btn p-button-success p-button-rounded p-button-outlined'};
     const cancelOptions = {icon: 'pi pi-fw pi-times', iconOnly: true, className: 'custom-cancel-btn p-button-danger p-button-rounded p-button-outlined'};
-
+    
     return (
         <div>
 
-            <h1>Yemek Kategorisi</h1>
-             <div className="card">
-                <S.TextCotainerF>
-                <S.TextCotainerT>Yemek Kategorisi Oluştur</S.TextCotainerT>
-                </S.TextCotainerF>
+            <h1>Oluştur</h1>
+            <Toast ref={toast}></Toast>
+             <S.ContainerCard>
                     <div className="p-fluid">
                         <div className="p-field">
-                            <label htmlFor="firstname1">Firstname</label>
+                        <h4>Restauran</h4>
+                <Dropdown value={selectedCity1} options={cities} onChange={onCityChange} optionLabel="name" placeholder="Select a City" />
+                        </div>
+                        <div className="p-field">
+                            <h4>Yemek Adı</h4>
                             <InputText id="firstname1" type="text"/>
                         </div>
                         <div className="p-field">
-                            <label htmlFor="lastname1">Lastname</label>
+                            <h4>Yemek Açıklaması</h4>
                             <InputText id="lastname1" type="text"/>
                         </div>
                     </div>
-            
-            
-
-            <h5>Template</h5>
-            <FileUpload ref={fileUploadRef} name="demo[]" url="./upload.php" multiple accept="image/*" maxFileSize={1000000}
+            <FileUpload ref={fileUploadRef} name="image" url="./" multiple accept="image/*" maxFileSize={1000000}
                 onUpload={onTemplateUpload} onSelect={onTemplateSelect} onError={onTemplateClear} onClear={onTemplateClear}
-                headerTemplate={headerTemplate} itemTemplate={itemTemplate} emptyTemplate={emptyTemplate}
-                chooseOptions={chooseOptions} uploadOptions={uploadOptions} cancelOptions={cancelOptions} />
-
-
+                headerTemplate={headerTemplate} itemTemplate={itemTemplate} emptyTemplate={emptyTemplate} chooseOptions={chooseOptions} 
+                uploadOptions={uploadOptions} cancelOptions={cancelOptions} />
+                <div className="p-fluid">
+                        <div className="card">
+                <h4>Yemek Kategorisi</h4>
+                <Dropdown value={selectedCity1} options={cities} onChange={onCityChange} optionLabel="name" placeholder="Yemek Kategorisi" />
+                <h4>Eklentileri Seç</h4>
+                <Dropdown value={selectedCity1} options={cities} onChange={onCityChange} optionLabel="name" placeholder="Eklentileri Seç" />
                 </div>
-            
+            </div>
+            <div className="p-grid p-fluid">
+                    <div className="p-field p-col-12 p-md-3">
+                        <h4> Fiyat</h4>
+                        <InputNumber id="stacked" value={value17} onValueChange={(e) => setValue17(e.value)} showButtons mode="currency" currency="TRY" />
+                    </div>
+                    <div className="p-field p-col-12 p-md-3">
+                        <h4> İndirimli Fiyat</h4>
+                        <InputNumber id="stacked" value={value17} onValueChange={(e) => setValue17(e.value)} showButtons mode="currency" currency="TRY" />
+                    </div>
+            </div>
+            <div>
+                
+                <h4>Saf Sebze Mi</h4>
+                <InputSwitch checked={vegi} onChange={(e) => setVegi(e.value)} />
+                
+                <h4>Öne Çıkma</h4>
+                <InputSwitch checked={featured} onChange={(e) => setFeatured(e.value)} />
+
+                <h4>Aktif</h4>
+                <InputSwitch checked={active} onChange={(e) => setActive(e.value)} />
+            </div>
+
+            <S.SubmitBtn>
+
+                <Button type="button" label="Submit"/>
+            </S.SubmitBtn>
+            </S.ContainerCard>
         </div>
     )
 }
--- pages/foods/create-foods.tsx/style.tsx
+++ pages/foods/create-foods.tsx/style.tsx
@@ -1,45 +1,15 @@
 import styled from "styled-components";
-// export const Table = styled(DataTable)`
-//     .datatable-filter-demo .p-paginator .p-paginator-current {
-//     margin-right: auto !important;
-// }
-// .table-header {
-//     display: flex;
-//     justify-content: space-between;
-// }
-// .food-image {
-//     width: 100px;
-//     box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-// }
-// `;
 
-
-export const TextCotainerT = styled.h2`
-    position:fixed;
-    top: 70px;
-    margin-left:30px;     
-
-
+export const ContainerCard = styled.div`
         
-`;
-
-export const imgContainer  = styled.img`
-        position:fixed;
-        top:-30px;
-        width:50px;
-        height:50px;
-`;
-
-export const TextCotainerF = styled.div`
-        
-        color:white;
         width:100%;
-        height:50px;
-        background-color:#0066ff;
+        height:90%;
         border-radius:10px;
         
 `;
 
-export const Span2 = styled.span`
-        background-color:red;
+export const SubmitBtn = styled.div`
+        
+        margin-top:60px;
+        
 `;
