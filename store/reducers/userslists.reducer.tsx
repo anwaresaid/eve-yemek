3eb0a10a18e8eee3de5b89f887976cc37d019e72
Usama129 <usama.humayun@ug.bilkent.edu.tr>
2021-05-25 13:20:28 +0300

    Fixed issue with state type in userlists.reducer

--- store/reducers/userslists.reducer.tsx
+++ store/reducers/userslists.reducer.tsx
@@ -32,7 +32,7 @@ export const customerListReducer = (state = {customers:{items:[]}}, action) => {
 
 }
 
-export const restaurantOwnerListReducer = (state = initialState, action) => {
+export const restaurantOwnerListReducer = (state = {restaurantOwners:{items:[]}}, action) => {
     const { type, payload } = action;
 
     switch (action.type) {
@@ -62,7 +62,7 @@ export const restaurantOwnerListReducer = (state = initialState, action) => {
 
 }
 
-export const deliveryScoutListReducer = (state = initialState, action) => {
+export const deliveryScoutListReducer = (state = {deliveryScouts:{items:[]}}, action) => {
     const { type, payload } = action;
 
     switch (action.type) {
@@ -92,7 +92,7 @@ export const deliveryScoutListReducer = (state = initialState, action) => {
 
 }
 
-export const customerServiceListReducer = (state = initialState, action) => {
+export const customerServiceListReducer = (state = {customerService:{items:[]}}, action) => {
     const { type, payload } = action;
 
     switch (action.type) {
