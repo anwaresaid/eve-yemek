043d5ceb40a4eacc5387318a7462176e9bef16f0
anwaresaid <51533569+anwaresaid@users.noreply.github.com>
2021-05-26 16:54:25 +0300

    created create addon page

--- store/services/addons.service.tsx
+++ store/services/addons.service.tsx
@@ -6,4 +6,9 @@ export default class AddonService{
        const {data} = await axios.get('https://dev.eve-yemek.com/add-ons');
         return data
     }
+
+    async createAddons(addons){
+       const {data} = await axios.post('https://dev.eve-yemek.com/add-ons',{...addons});
+        return data
+    }
 }
