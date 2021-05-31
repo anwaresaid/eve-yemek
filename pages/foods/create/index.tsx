36649af85155822fcd298b9339c4ef6bc0c72136
Usama Humayun <usama.humayun@ug.bilkent.edu.tr>
2021-05-31 14:11:25 +0300

    Created a StandardFileUpload component, changed Food create page to use said StandardFileUpload

--- pages/foods/create/index.tsx
+++ pages/foods/create/index.tsx
@@ -18,13 +18,13 @@ import { MultiSelect } from 'primereact/multiselect';
 import {RootState} from 'typesafe-actions';
 import { useFormik } from 'formik';
 import classNames from 'classnames'
+import StandardFileUpload from '../../../components/inputs/fileUpload';
 
 
 export const Index = () => {
 
     const [totalSize, setTotalSize] = useState(0);
     const toast = useRef(null);
-    const fileUploadRef = useRef(null);
     const [addonsName, setAddonsName] = useState(null);
     const [foodCategoryName, setFoodCategoryName] = useState(null);
     const [restaurantName, setRestaurantName] = useState(null);
@@ -144,84 +144,6 @@ export const Index = () => {
     //  }
 
 
-
-//image upload functions
-    const onTemplateSelect = (e:any) => {
-        let _totalSize = totalSize;
-        // e.files.map(file => {
-        //     _totalSize += file.size;
-        // });
-        for(let i = 0 ; i<e.files.length;i++){
-            _totalSize += e.files[i].size;
-        }
-        setTotalSize(_totalSize);
-
-    }
-
-
-    const onTemplateUpload = (e) => {
-        let _totalSize = 0;
-        // setFile(e.files[0]);
-        formik.values.file=e.files[0];
-        e.files.forEach(file => {
-            _totalSize += (file.size || 0);
-        });
-
-        setTotalSize(_totalSize);
-        toast.current.show({severity: 'info', summary: 'Success', detail: 'File Uploaded'});
-    }
-
-    const onTemplateRemove = (file, callback) => {
-        setTotalSize(totalSize - file.size);
-        callback();
-    }
-
-    const onTemplateClear = () => {
-        setTotalSize(0);
-    }
-
-    const headerTemplate = (options) => {
-        const { className, chooseButton, uploadButton, cancelButton } = options;
-        const value = totalSize/20000;
-        const formatedValue = fileUploadRef && fileUploadRef.current ? fileUploadRef.current.formatSize(totalSize) : '0 B';
-
-        return (
-            <div className={className} style={{backgroundColor: 'transparent', display: 'flex', alignItems: 'center'}}>
-                {chooseButton}
-                {uploadButton}
-                {cancelButton}
-                <ProgressBar value={value} displayValueTemplate={() => `${formatedValue} / 2 MB`} style={{width: '300px', height: '20px', marginLeft: 'auto'}}></ProgressBar>
-            </div>
-        );
-    }
-
-    const itemTemplate = (file, props) => {
-        return (
-            <div className="p-d-flex p-ai-center p-flex-wrap">
-                <div className="p-d-flex p-ai-center" style={{width: '40%'}}>
-                    <img alt={file.name} role="presentation" src={file.objectURL} width={100} />
-                    <span className="p-d-flex p-dir-col p-text-left p-ml-3">
-                        {file.name}
-                        <small>{new Date().toLocaleDateString()}</small>
-                    </span>
-                </div>
-                <Tag value={props.formatSize} severity="warning" className="p-px-3 p-py-2" />
-                <Button type="button" icon="pi pi-times" className="p-button-outlined p-button-rounded p-button-danger p-ml-auto" onClick={() => onTemplateRemove(file, props.onRemove)} />
-            </div>
-        )
-    }
-
-    const emptyTemplate = () => {
-        return (
-            <div className="p-d-flex p-ai-center p-dir-col">
-                 <i className="pi pi-image p-mt-3 p-p-5" style={{'fontSize': '5em', borderRadius: '50%', backgroundColor: 'var(--surface-b)', color: 'var(--surface-d)'}}></i>
-                <span style={{'fontSize': '1.2em', color: 'var(--text-color-secondary)'}} className="p-my-5">Drag and Drop Image Here</span>
-            </div>
-        )
-    }
-    const chooseOptions = {icon: 'pi pi-fw pi-images', iconOnly: true, className: 'custom-choose-btn p-button-rounded p-button-outlined'};
-    const uploadOptions = {icon: 'pi pi-fw pi-cloud-upload', iconOnly: true, className: 'custom-upload-btn p-button-success p-button-rounded p-button-outlined'};
-    const cancelOptions = {icon: 'pi pi-fw pi-times', iconOnly: true, className: 'custom-cancel-btn p-button-danger p-button-rounded p-button-outlined'};
     function multiSelect(){
         if(addonsName != null)
             return(
@@ -257,11 +179,14 @@ export const Index = () => {
                             {getFormErrorMessage('description')}
                         </div>
                     </div>
-                    <FileUpload ref={fileUploadRef} id="file" name="file" url="./" multiple accept="image/*" maxFileSize={1000000}
-                        onUpload={onTemplateUpload} onSelect={onTemplateSelect} onError={onTemplateClear} onClear={onTemplateClear}
-                        headerTemplate={headerTemplate} itemTemplate={itemTemplate} emptyTemplate={emptyTemplate} chooseOptions={chooseOptions}
-                        uploadOptions={uploadOptions} cancelOptions={cancelOptions} className={classNames({ 'p-invalid': isFormFieldValid('file') })} />
-                          <label htmlFor="file" className={classNames({ 'p-error': isFormFieldValid('file') })}></label>
+                        <StandardFileUpload 
+                            totalSize={totalSize} 
+                            setTotalSize={setTotalSize} 
+                            setFile={(file)=>{formik.values.file=file}}
+                            showSuccess={()=>{toast.current.show({severity: 'info', summary: 'Success', detail: 'File Uploaded'});}}
+                            >   
+                        </StandardFileUpload>
+                        <label htmlFor="file" className={classNames({ 'p-error': isFormFieldValid('file') })}></label>
                                         {getFormErrorMessage('file')}
                     <div className="p-fluid">
                         <div className="card">
