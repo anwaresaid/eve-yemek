1722cfceebe32ff82f6211c154f87bd2425d629e
Abdulkader El Rawas <63848905+abdulkaderelrawas@users.noreply.github.com>
2021-06-01 11:16:22 +0300

    coupons page init w/ redux

---
+++ pages/coupons/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import AddonsTable from '../../components/tables/addonsTable';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from 'typesafe-actions';
+import { ProgressSpinner } from 'primereact/progressspinner';
+import { listCoupons } from '../../store/actions/coupons.action';
+
+const index = () => {
+  const [addon, setAddon] = useState(null);
+  const [addonCategory, setAddonCategory] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const res = useSelector((state: RootState) => state.listCoupons);
+  const { loading, success, coupons } = res;
+
+  useEffect(() => {
+    if (!coupons) dispatch(listCoupons());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {!loading && (
+        <AddonsTable addons={addon} addonCat={addonCategory}></AddonsTable>
+      )}
+      {loading && <ProgressSpinner />}
+    </div>
+  );
+};
+
+export default index;
