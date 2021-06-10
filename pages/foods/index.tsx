e37de36cb09fe3330ddb09f3a235c7c87b1ba129
anwaresaid <51533569+anwaresaid@users.noreply.github.com>
2021-06-10 11:52:24 +0300

    added h1 to the foods table, sync data on food edit with the backend data

--- pages/foods/index.tsx
+++ pages/foods/index.tsx
@@ -21,7 +21,8 @@ const FoodsList =  () => {
     
      return (
         <div id="foodsCategoryTable">
-            {!loading && foods && <FoodsTable foods={foods.items}></FoodsTable>}
+            {console.log(foods)}
+            {!loading && foods && <><h1>Foods</h1><FoodsTable foods={foods.items}></FoodsTable></>}
             {loading && <ProgressSpinner/>}
         </div>
     );
