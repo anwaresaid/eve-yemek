d0d8d507603975ab9e00ab8b8a4b8033cbe5f10d
anwaresaid <51533569+anwaresaid@users.noreply.github.com>
2021-06-15 15:12:58 +0300

    conflict resolved

--- components/DataInputForms/MealDataInput/FoodCategoryDataInput.tsx
+++ components/DataInputForms/MealDataInput/FoodCategoryDataInput.tsx
@@ -10,9 +10,8 @@ import { useRouter } from 'next/router';
 import { getFoodCategoryDetails, updateFoodCategory } from '../../../store/actions/foodCategory.action';
 import { foodCategoryTypes } from '../../../store/types/foodCategory.type';
 import { useFormik } from 'formik';
-import classNames from 'classnames'
-import InputContainer from '../../../components/inputs/inputContainer';
-import StandardFileUpload from '../../../components/inputs/fileUpload';
+import InputContainer from '../../inputs/inputContainer';
+import StandardFileUpload from '../../inputs/fileUpload';
 import { i18n } from '../../../language';
 import FormColumn from "../../inputs/formColumn";
 import InputGroup from "../../inputs/inputGroup";
@@ -36,6 +35,9 @@ export const FoodCategoryDataInput = (props) => {
   const getFormErrorMessage = (name) => {
       return isFormFieldValid(name) && <small className="p-error">{formik.errors[name]}</small>;
   };
+   const foodCategoryCreate = useSelector((state:RootState) => state.createFoodCategory);
+  const { success } = foodCategoryCreate;
+
   const formik = useFormik({
     initialValues:{
         name: '',
@@ -83,49 +85,41 @@ export const FoodCategoryDataInput = (props) => {
     isFormFieldValid
   }
 
-
   return (
     <div id="edit_food_categories">
-      <h1 id="editHeader">Kategori Detayi</h1>
+      <h1 id="editHeader">{i18n.t('update')}</h1>
       <Toast id="toastMessage" ref={toast}></Toast>
       <S.ContainerCard id="container">
-        <form id="editForm" onSubmit={formik.handleSubmit}>
-            <div className="p-grid">
-        <FormColumn divideCount={3}>
-        <InputGroup>
-          {/* <div id="nameDiv" className='p-field'>
-              <h4>Kategori Adı</h4>
-              <InputText id='name' name='name' value={formik.values.name} onChange={formik.handleChange} type='text' className={classNames({ 'p-invalid': isFormFieldValid('name') })} />
-              <label id="errorName" htmlFor="name" className={classNames({ 'p-error': isFormFieldValid('name') })}></label>
-              {getFormErrorMessage('name')}
-            </div> */}
-        <InputContainer label={i18n.t('name')} name="name" formiks={inputFormiks} component={InputText} iprops={{
-            value: formik.values.name,
-            onChange: formik.handleChange,
-            }} />
-              <InputContainer label="Resim" name="file" formiks={inputFormiks} component={StandardFileUpload} iprops={{
-                  setFile:(image)=>{ formik.values.image=image },
-                  showSuccess:()=>{toast.current.show({severity: 'info', summary: 'Success', detail: 'File Uploaded'});}
-                }}/>
-        
-        </InputGroup>
-        
-        <InputContainer label={i18n.t('active')} name="active" noAutoCol12 formiks={inputFormiks} component={InputSwitch} iprops={{
-            value: formik.values.active,
-            checked: formik.values.active,
-            onChange: formik.handleChange
-                            }} />
-          {/* <div>
-            <h4>Aktif Mi</h4>
-            <InputSwitch checked={formik.values.active} onChange={formik.handleChange} />
-          </div> */}
-          <S.SubmitBtn id="btnContainer">
-            <Button id="editBtn" type='submit' label={i18n.t('submit')}/>
-          </S.SubmitBtn>
-          </FormColumn>
-          </div>
-        </form>
-      </S.ContainerCard>
+            <form id="editForm" onSubmit={formik.handleSubmit}>
+                <div className="p-grid">
+            <FormColumn divideCount={3}>
+            <InputGroup>
+            <InputContainer label={i18n.t('name')} name="name" formiks={inputFormiks} component={InputText} iprops={{
+                value: formik.values.name,
+                onChange: formik.handleChange,
+                }} />
+            </InputGroup>
+                  <InputContainer label="Resim" name="file" formiks={inputFormiks} component={StandardFileUpload} iprops={{
+                      setFile:(image)=>{ formik.values.image=image },
+                      showSuccess:()=>{toast.current.show({severity: 'info', summary: 'Success', detail: 'File Uploaded'});}
+                    }}/>
+                    
+                <InputContainer label={i18n.t('active')} name="active" noAutoCol12 formiks={inputFormiks} component={InputSwitch} iprops={{
+                  value: formik.values.active,
+                  checked: formik.values.active,
+                  onChange: formik.handleChange
+                }} />
+
+
+                <S.SubmitBtn id="btnContainer">
+                  <Button id="editBtn" type='submit' label={i18n.t('submit')}/>
+                </S.SubmitBtn>
+
+              </FormColumn>
+              </div>
+            </form>
+          </S.ContainerCard>
+
     </div>
   );
 };
