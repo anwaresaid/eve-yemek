767e91ac6cb945018e297e2609c8f0cbd7e9eb8b
Feras Allaou <feras.allaou@gmail.com>
2021-06-25 11:25:41 +0300

    Fixed Config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+    env: {
+        NEXT_PUBLIC_API_MODE: 'dev',
+        NEST_API_URL: 'https://devapi.eve-yemek.com',
+        NEXT_PUBLIC_FCM_REQUEST_KEY: 'AAAAVZiHLUQ:APA91bHhcjfRRvhY4JjcQL8Nea8xaJg6RN2oh3id4ta3qgwEmd-AmJweVMEjDVPiqk0lBtT0nWKefrscFqNltLrlZ03rOyg6Eud9cJW4SeNASbKweY-UupNtJFFSY_jEZkmjVMSXJLvo',
+      },
     future: {
         webpack5: true,
     },
