18615942074a374f2874ee62e6f57cd8a379c367
Abdulkader El Rawas <63848905+abdulkaderelrawas@users.noreply.github.com>
2021-07-09 16:11:57 +0300

    WIP: add socket and receive notifications on new orders for restaurant owners

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,115 +1,131 @@
-import Head from "next/head";
+import Head from 'next/head';
 import { hotjar } from 'react-hotjar';
-
-import "primereact/resources/themes/saga-blue/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
-import "primeflex/primeflex.css";
-import React, { useEffect, useState } from "react";
-import Sidebar from "../components/Sidebar";
-import GlobalStyle from "../styles/core/global";
-import { Provider } from "react-redux";
-import store from "../store/store";
-import auth from "../helpers/core/auth";
-import Error from "next/error";
-import { i18n } from "../language";
-import TopBar from "../components/TopBar";
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import React, { useEffect, useState } from 'react';
+import Sidebar from '../components/Sidebar';
+import GlobalStyle from '../styles/core/global';
+import { Provider, useSelector } from 'react-redux';
+import store from '../store/store';
+import auth from '../helpers/core/auth';
+import Error from 'next/error';
+import { i18n } from '../language';
+import TopBar from '../components/TopBar';
+import { useSocket } from '../helpers/socket';
+import { RootState } from 'typesafe-actions';
 
 function MyApp(props) {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [error, setError] = useState(false);
+  const [authCheckFinish, setAuthCheckFinish] = useState(false);
+  const [hideBar, setHideBar] = useState(true);
 
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [error, setError] = useState(false);
-    const [authCheckFinish, setAuthCheckFinish] = useState(false);
-    const [hideBar, setHideBar] = useState(true);
+  let socket: any;
 
-    
-    useEffect(() => {
-        auth.init();
-        authCheck();
-        setLoggedIn(auth.loggedIn);
-        document?.documentElement?.setAttribute("lang", i18n.language);
-        document?.documentElement?.setAttribute("dir", i18n.dir());
-        hotjar.initialize(parseInt(process.env.NEXT_PUBLIC_HOTJAR_TRACKING_ID), 6);
-    }, []);
+  useEffect(() => {
+    auth.init();
+    authCheck();
+    setLoggedIn(auth.loggedIn);
+    document?.documentElement?.setAttribute('lang', i18n.language);
+    document?.documentElement?.setAttribute('dir', i18n.dir());
+    hotjar.initialize(parseInt(process.env.NEXT_PUBLIC_HOTJAR_TRACKING_ID), 6);
+    if (auth.loggedIn && auth.hasRoles('restaurant_owner')) {
+      console.log(auth.user)
+      socket = useSocket();
+      if (socket) {
+        socket.on(
+          `order.created.${auth.user.restaurants_ids}`,
+          ({ payload }) => {
+            console.log('new order incoming: ', payload);
+          }
+        );
+      }
+    }
+  }, []);
 
-    const authCheck = () => {
-        if (auth.loggedIn && window.location.pathname === "/auth/login") {
-            window.location.replace("/");
-            return;
+  const authCheck = () => {
+    if (auth.loggedIn && window.location.pathname === '/auth/login') {
+      window.location.replace('/');
+      return;
+    }
+
+    if (window.location.pathname !== '/auth/login') {
+      if (!auth.loggedIn || !auth.user?.roles) {
+        window.location.replace('/auth/login');
+        return;
+      }
+
+      //Check is route direcly allowed
+      if (!auth.isAllowedRoute(window.location.pathname)) {
+        //If not, then check with id, like [/orders/]123456 find inside [*] and check with that
+        if (
+          !auth.isAllowedRoute(
+            window.location.pathname.substr(
+              0,
+              window.location.pathname.indexOf('/', 3) // The second "/"
+            )
+          )
+        ) {
+          setError(true);
         }
+      }
+    }
 
-        if (window.location.pathname !== "/auth/login") {
-            if (!auth.loggedIn || !auth.user?.roles) {
-                window.location.replace("/auth/login");
-                return;
-            }
+    setAuthCheckFinish(true);
+  };
 
-            //Check is route direcly allowed
-            if (!auth.isAllowedRoute(window.location.pathname)) {
+  const renderComp = () => {
+    if (!authCheckFinish) {
+      //TODO: Loading comp
+      return (() => <>Loading</>)();
+    }
 
-                //If not, then check with id, like [/orders/]123456 find inside [*] and check with that
-                if (
-                    !auth.isAllowedRoute(
-                        window.location.pathname.substr(
-                            0,
-                            window.location.pathname.indexOf("/", 3) // The second "/"
-                        )
-                    )
-                ) {
-                    setError(true);
-                }
-            }
-        }
+    if (error) {
+      return <Error id='errorStatusCode' statusCode={404} />;
+    }
 
-        setAuthCheckFinish(true);
-    };
-
-    const renderComp = () => {
-        if (!authCheckFinish) {
-            //TODO: Loading comp
-            return (() => <>Loading</>)();
-        }
-
-        if (error) {
-            return <Error id='errorStatusCode' statusCode={404} />;
-        }
-
-        if (loggedIn) {
-            return (
-                <>
-                    <Sidebar open={hideBar} setOpen={setHideBar} />
-                    <TopBar hideBar={hideBar} setHideBar={setHideBar} />
-
-                    <div className={"main-context" + (hideBar ? "-showBar" : "-hideBar")}>
-                        <props.Component {...props.pageProps} />
-                    </div>
-                </>
-            );
-        }
-
-        return <props.Component {...props.pageProps} />;
-    };
-    return (
+    if (loggedIn) {
+      return (
         <>
-            <Head >
-                <link id='logoLink' rel="shortcut icon" href="/images/logos/logo.png"></link>
-                <title id='adminPanelTitle'>Eve Yemek - Admin Panel</title>
-                <meta charSet="utf-8" />
-                <meta
-                    name="viewport"
-                    content="initial-scale=1.0, width=device-width"
-                />
-                <meta id='adminPanelDescription' name="description" content="Eve yemek admin panel" />
-            </Head>
-            <Provider store={store}>
-                <div id='appDiv' className="app">
-                    <GlobalStyle id='globalStyle' open={hideBar} setOpen={setHideBar} />
+          <Sidebar open={hideBar} setOpen={setHideBar} />
+          <TopBar hideBar={hideBar} setHideBar={setHideBar} />
 
-                    {renderComp()}
-                </div>
-            </Provider>
+          <div className={'main-context' + (hideBar ? '-showBar' : '-hideBar')}>
+            <props.Component {...props.pageProps} />
+          </div>
         </>
-    );
+      );
+    }
+
+    return <props.Component {...props.pageProps} />;
+  };
+  return (
+    <>
+      <Head>
+        <link
+          id='logoLink'
+          rel='shortcut icon'
+          href='/images/logos/logo.png'
+        ></link>
+        <title id='adminPanelTitle'>Eve Yemek - Admin Panel</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta
+          id='adminPanelDescription'
+          name='description'
+          content='Eve yemek admin panel'
+        />
+      </Head>
+      <Provider store={store}>
+        <div id='appDiv' className='app'>
+          <GlobalStyle id='globalStyle' open={hideBar} setOpen={setHideBar} />
+          {renderComp()}
+        </div>
+      </Provider>
+    </>
+  );
 }
 
 export default MyApp;
