41108b0eb459f8f399e80d488e85eceaf1cf4a77
Usama129 <usama@alumni.bilkent.edu.tr>
2021-07-12 12:30:15 +0300

    Fixed typing error

--- language/locales/ar/translations.js
+++ language/locales/ar/translations.js
@@ -281,6 +281,6 @@ export const translations = {
   replaceImageWithDefault: 'استبدال الصورة بالصورة الافتراضية',
   thisFeatureIsForAdminsToReplaceLegacyImages: 'هذه الميزة للمشرفين لاستبدال الصور القديمة',
   send: 'إرسال',
-  havingTroubleLoggingIn: 'هل تواجه مشكلة في تسجيل الدخول؟'.\,
+  havingTroubleLoggingIn: 'هل تواجه مشكلة في تسجيل الدخول؟',
   executeTransfer: 'تنفيذ التحويل'
 };
