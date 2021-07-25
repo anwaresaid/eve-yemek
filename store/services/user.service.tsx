0b4d5dd573094d7e41649e8012707a1c26355e8b
Abdulkader El Rawas <63848905+abdulkaderelrawas@users.noreply.github.com>
2021-07-25 13:15:58 +0300

    removed unused code in user service

--- store/services/user.service.tsx
+++ store/services/user.service.tsx
@@ -1,47 +1,4 @@
 import axios from '../../helpers/_axios';
-
-// const UserService = function () {
-//   const login = async (email: string, password: string, remember: boolean) => {
-//     if (!email || !password) {
-//       return { ok: false, err: 'Email or password is empty' };
-//     }
-
-//     return axios
-//       .post('/users/login', {
-//         email,
-//         password,
-//         remember,
-//       })
-//       .then((res) => {
-//         return { ok: true, data: res.data.data };
-//       })
-//       .catch((err) => {
-//         return { ok: false, err: err?.data || err?.response?.data };
-//       });
-//   };
-
-//   const changePassword = async (new_password: string, password: string) => {
-//     const {
-//       data: { data },
-//     } = await axios.put(`/users/change-password`, { new_password, password });
-//     return data;
-//   };
-
-//   const resetPasswordRequest = async (email: string) => {
-//     const {
-//       data: { data },
-//     } = await axios.post('/users/resetPasswordRequest', { email });
-
-//     return data;
-//   };
-
-//   return Object.freeze({
-//     login,
-//     changePassword,
-//     resetPasswordRequest,
-//   });
-// };
-
 export default class UserService {
   public async login(email: string, password: string, remember: boolean) {
     if (!email || !password) {
@@ -77,5 +34,3 @@ export default class UserService {
       return data;
   }
 }
-
-// export default UserService();
