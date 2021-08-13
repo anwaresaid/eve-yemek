ddae4e32dd7f7e04e7eb7a027361a6915d659d48
anwaresaid <51533569+anwaresaid@users.noreply.github.com>
2021-08-13 11:49:18 +0300

    solving the conflinct in constants

--- helpers/constants.tsx
+++ helpers/constants.tsx
@@ -80,5 +80,4 @@ export const allMenuItems: any = [
     { label: i18n.t("settings"), url: "/settings", roles: ["admin", "customer_service", "restaurant_owner"] },
     { label: i18n.t("transfers"), url: "/transfers", roles: ["admin"] },
     { label: i18n.t("logout"), url: "/auth/logout", roles: ["admin", "restaurant_owner", "customer_service"] }
-
 ];
